6b0150c6623d6ad0484cda2e7f15fe64efe8cc94
Mladoneczki <padi.mladoneczki@gmail.com>
2024-09-28 15:06:00 +0200

    termekLista

---
+++ termekLista.js
@@ -0,0 +1,64 @@
+export const termekLista=[
+    {
+        "image": "https://unsplash.com/photos/bowl-of-red-and-black-berries-uZjF45POZP4",
+        "title": "Classic Fruit Gummies",
+        "description": "Colorful fruit-flavored gummies in a variety of shapes, such as bears, worms, and rings."
+    },
+    {
+        "image": "https://unsplash.com/photos/gummy-worms-SGI0i5sHCL8",
+        "title": "Sour Gummies",
+        "description": "Tangy, sugar-coated gummies in intense sour flavors like lemon, lime, and cherry."
+    },
+    {
+        "image": "https://tse3.mm.bing.net/th?id=OIP.rTdlVDygzsv8AA6XXR2xcQHaFj&pid=Api&P=0&h=220",
+        "title": "Gummy Cola Bottles",
+        "description": "Cola-flavored gummy candies shaped like mini soda bottles with a fizzy finish."
+    },
+    {
+        "image": "https://unsplash.com/photos/a-group-of-orange-candies-sitting-next-to-a-bottle-LghhiLZX7e8",
+        "title": "Vegan Gummies",
+        "description": "Plant-based gummy bears made with natural fruit juices and without gelatin."
+    },
+    {
+        "image": "https://unsplash.com/photos/Zp7ebyti3MU",
+        "title": "Gummy Rings",
+        "description": "Soft gummy rings in peach, apple, and watermelon flavors, with a sweet and sour coating."
+    },
+    {
+        "image": "https://unsplash.com/photos/BfphcCvhl6E",
+        "title": "Gummy Worms",
+        "description": "Long, colorful gummy worms in fruity flavors, perfect for a fun snack."
+    },
+    {
+        "image": "https://unsplash.com/photos/RkBTPqPEGDo",
+        "title": "Vitamin Gummies",
+        "description": "Gummies infused with essential vitamins, providing a tasty and healthy boost."
+    },
+    {
+        "image": "https://unsplash.com/photos/Z7wRVvXP-0E",
+        "title": "Gummy Sharks",
+        "description": "Blue and white gummy sharks with a sweet and fruity flavor."
+    },
+    {
+        "image": "https://unsplash.com/photos/pkK5EV9ZLqk",
+        "title": "Gummy Bears",
+        "description": "Classic gummy bears in a variety of fruit flavors, from orange to raspberry."
+    },
+    {
+        "image": "https://unsplash.com/photos/AzCzP3yWZmA",
+        "title": "Sour Patch Kids",
+        "description": "Sour, then sweet chewy gummies shaped like little kids, in a variety of bold flavors."
+    },
+    {
+        "image": "https://unsplash.com/photos/U3aF7hgUSrk",
+        "title": "Gummy Dinosaurs",
+        "description": "Fun-shaped gummies in the form of dinosaurs, available in fruity flavors."
+    },
+    {
+        "image": "https://unsplash.com/photos/Q69E5L-QKjs",
+        "title": "Rainbow Gummies",
+        "description": "Multi-colored, layered gummies with a combination of fruity flavors in every bite."
+    }
+]
+
+
